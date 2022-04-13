3e6fac2f5da243b13da10e8e4be26d292d04aabd
YuukiSakihiro <k021g1259@g.neec.ac.jp>
2022-04-13 10:51:02 +0900

    プロジェクト ファイルを追加します。

---
+++ scene/GameScene.cpp
@@ -0,0 +1,81 @@
+﻿#include "GameScene.h"
+#include "TextureManager.h"
+#include <cassert>
+
+using namespace DirectX;
+//コンストラクタ
+GameScene::GameScene() {}
+//デストラクタ
+GameScene::~GameScene() { delete Sprite_; }
+
+void GameScene::Initialize() {
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+	debugText_ = DebugText::GetInstance();
+	//failを指定
+
+	textureHandle_ = TextureManager::Load("FF.png");
+	
+	Sprite_ = Sprite::Create(textureHandle_, {10, 5});
+
+}
+/// <summary>
+//更新
+
+/// </summary>
+void GameScene::Update() 
+{ XMFLOAT2 position =Sprite_->GetPosition();
+	position.x += 2.0f;
+	position.y += 1.0f;
+	Sprite_->SetPosition(position);
+}
+
+void GameScene::Draw() {
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+	Sprite_->Draw();
+	// デバッグテキストの描画
+	debugText_->DrawAll(commandList);
+	//
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
